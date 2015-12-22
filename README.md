4fcb187390358e24f5db6351610b489ec782c5bb
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@mykolab.com>
2015-12-22 10:10:10 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# JSONAPIMapper
+A mapping framework for http://jsonapi.org
+
+[![Carthage compatible](https://img.shields.io/badge/Carthage-compatible-4BC51D.svg?style=flat)](https://github.com/Carthage/Carthage)
+
+![platform ios](https://img.shields.io/badge/platform-iOS-lightgrey.svg)
---
+++ Source/JSONAPIMapper.h
@@ -0,0 +1,19 @@
+//
+//  JSONAPIMapper.h
+//  JSONAPIMapper
+//
+//  Created by Giorgos Tsiapaliokas on 22/12/15.
+//  Copyright © 2015 Giorgos Tsiapaliokas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSONAPIMapper.
+FOUNDATION_EXPORT double JSONAPIMapperVersionNumber;
+
+//! Project version string for JSONAPIMapper.
+FOUNDATION_EXPORT const unsigned char JSONAPIMapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONAPIMapper/PublicHeader.h>
+
+
