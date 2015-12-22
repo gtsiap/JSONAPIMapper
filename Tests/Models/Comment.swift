9eeec2d8a5b4643c7cfc801b817aa3706a91eeac
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@mykolab.com>
2015-12-22 15:14:29 +0200

    fix license

--- Tests/Models/Comment.swift
+++ Tests/Models/Comment.swift
@@ -1,4 +1,3 @@
-
 // Copyright (c) 2015 Giorgos Tsiapaliokas <giorgos.tsiapaliokas@mykolab.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -18,14 +17,6 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-
-//
-//  Comment.swift
-//  GtsiapKit
-//
-//  Created by Giorgos Tsiapaliokas on 8/29/15.
-//  Copyright © 2015 Giorgos Tsiapaliokas. All rights reserved.
-//
 
 import JSONAPIMapper
 
